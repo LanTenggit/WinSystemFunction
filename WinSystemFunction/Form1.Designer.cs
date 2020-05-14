6edd16cde397d54001f57a11257b0d370e15abdf
lc <1784455078@qq.com>
2020-05-14 10:06:40 +0800

    创建开机启动页面

---
+++ WinSystemFunction/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WinSystemFunction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bn_sure = new System.Windows.Forms.Button();
+            this.bn_cancel = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "程序名称";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "程序路径";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "开启/关闭";
+            // 
+            // bn_sure
+            // 
+            this.bn_sure.Location = new System.Drawing.Point(49, 201);
+            this.bn_sure.Name = "bn_sure";
+            this.bn_sure.Size = new System.Drawing.Size(75, 23);
+            this.bn_sure.TabIndex = 3;
+            this.bn_sure.Text = "确定";
+            this.bn_sure.UseVisualStyleBackColor = true;
+            this.bn_sure.Click += new System.EventHandler(this.bn_sure_Click);
+            // 
+            // bn_cancel
+            // 
+            this.bn_cancel.Location = new System.Drawing.Point(187, 201);
+            this.bn_cancel.Name = "bn_cancel";
+            this.bn_cancel.Size = new System.Drawing.Size(75, 23);
+            this.bn_cancel.TabIndex = 4;
+            this.bn_cancel.Text = "取消";
+            this.bn_cancel.UseVisualStyleBackColor = true;
+            this.bn_cancel.Click += new System.EventHandler(this.bn_cancel_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(114, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 21);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(103, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "设置开机启动";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(133, 150);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 16);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "开启";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(187, 150);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 16);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.Text = "关闭";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 261);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bn_cancel);
+            this.Controls.Add(this.bn_sure);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button bn_sure;
+        private System.Windows.Forms.Button bn_cancel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ WinSystemFunction/Form1.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinSystemFunction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.radioButton1.Checked = true;
+        }
+
+
+
+        /// <summary> 
+        /// 开机启动项 
+        /// </summary> 
+        /// <param name=\"Started\">是否启动</param> 
+        /// <param name=\"name\">启动值的名称</param> 
+        /// <param name=\"path\">启动程序的路径</param> 
+        public static void RunWhenStart(bool Started, string name, string path)
+        {
+            RegistryKey HKLM = Registry.LocalMachine;
+            RegistryKey Run = HKLM.CreateSubKey(@"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\");
+            if (Started == true)
+            {
+                try
+                {
+                    Run.SetValue(name, path);
+                    HKLM.Close();
+                }
+                catch (Exception Err)
+                {
+                    MessageBox.Show(Err.Message.ToString(), "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                try
+                {
+                    Run.DeleteValue(name);
+                    HKLM.Close();
+                }
+                catch (Exception)
+                {
+                    // 
+                }
+            }
+        }
+
+
+        /// <summary>  
+        /// 更改是否开机自启动
+        /// </summary>  
+        /// <param name="isAutoStartup">是否开机自启动</param>  
+        public static void ChangeAutoStartUp(bool isAutoStartup,string ExePath,string ExeName)
+        {
+            string executablePath = Application.ExecutablePath; //可执行文件路径
+            string programName = Path.GetFileNameWithoutExtension(executablePath); //程序名称
+
+            executablePath = ExePath;
+            programName = ExeName;
+            //自启动注册表路径
+            string registryRunPath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+            try
+            {
+                bool isAdmin = IsAdmin();
+                //判断当前用户是否是管理员
+                RegistryKey registryKey = isAdmin
+                    ? Registry.LocalMachine.CreateSubKey(registryRunPath)
+                    : Registry.CurrentUser.CreateSubKey(registryRunPath);
+                if (registryKey == null) return;
+                if (isAutoStartup)
+                {
+                    registryKey.SetValue(programName, executablePath); //开启自启动
+                }
+                else
+                {
+                    registryKey.DeleteValue(programName, false); //关闭自启动
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        /// <summary>
+        /// 判断程序是否以管理员身份运行
+        /// </summary>
+        /// <returns></returns>
+        private static bool IsAdmin()
+        {
+            bool result = false;
+            try
+            {
+                System.Security.Principal.WindowsIdentity identity = System.Security.Principal.WindowsIdentity.GetCurrent();
+                System.Security.Principal.WindowsPrincipal principal = new System.Security.Principal.WindowsPrincipal(identity);
+                result = principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator);
+            }
+            catch (Exception e)
+            {
+                //捕获异常;
+            }
+            return result;
+        }
+        /// <summary>
+        /// 确定
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bn_sure_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ExeName = this.textBox1.Text.Trim();
+                string ExePath = this.textBox2.Text.Trim();
+
+                bool Exebool = true;
+                if (this.radioButton1.Checked)
+                {
+                    Exebool = true;
+                }
+                else
+                {
+                    Exebool = false;
+                }
+                ChangeAutoStartUp(Exebool, ExePath, ExeName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        
+        }
+        /// <summary>
+        /// 取消
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bn_cancel_Click(object sender, EventArgs e)
+        {
+
+            this.textBox1.Text = string.Empty;
+
+            this.textBox2.Text = string.Empty;
+        }
+    }
+}
