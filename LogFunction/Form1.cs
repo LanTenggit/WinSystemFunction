706a2960623dca005d4d730746ca4eea0d8cb8d2
LC <1784455078@qq.com>
2021-03-09 18:48:33 +0800

    log文件

---
+++ LogFunction/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogFunction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Logger.Instance.WriteLog("A");
+                Logger.Instance.WriteLog("b");
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.WriteException(ex);
+            }
+     
+        }
+    }
+}
---
+++ LogFunction/LogType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogFunction
+{
+    public enum LogType
+    {
+        All,
+        Information,
+        Debug,
+        Success,
+        Failure,
+        Warning,
+        Error
+    }
+}
